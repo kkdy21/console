3baffdd8815a4f33c474b722d1e66e6b87e75dcd
daeyeon ko <rowan@megazone.com>
2025-01-23 18:23:23 +0900

    test: test4
    Signed-off-by: daeyeon ko <rowan@megazone.com>

--- apps/web/src/router/type.ts
+++ apps/web/src/router/type.ts
@@ -3,3 +3,4 @@ import type { ROUTE_SCOPE } from '@/router/constant';
 export type RouteScopeType = keyof typeof ROUTE_SCOPE;
 
 export type RouteScopeType2 = 'test';
+export type RouteScopeType3 = 'test';
